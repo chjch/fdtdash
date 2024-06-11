d5d53b24efb24d09a873cdbf79cbc8153eb067b6
Changjie Chen <chj.chen@Hotmail.com>
2024-06-11 09:52:14 -0400

    add SceneLayer arcgis

--- jtflask/static/src/index.js
+++ jtflask/static/src/index.js
@@ -9,6 +9,7 @@ export { default as Map } from "@arcgis/core/Map";
 export { default as SceneView } from "@arcgis/core/views/SceneView";
 export { default as GeoJSONLayer } from "@arcgis/core/layers/GeoJSONLayer";
 export { default as FeatureLayer } from "@arcgis/core/layers/FeatureLayer";
+export { default as SceneLayer } from "@arcgis/core/layers/SceneLayer";
 // export { default as MapView } from "@arcgis/core/views/MapView";
 // export * as externalRenderers from '@arcgis/core/views/3d/externalRenderers';
 // export { externalRenderers };
