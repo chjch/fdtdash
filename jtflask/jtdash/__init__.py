018929d48df1e1fb0a9ffb343398677f19624768
Reggie <Reginald@ufl.edu>
2024-06-20 21:48:43 -0400

    Added new .css for side bar style.css Added base sidebar collapse - sidebar.py  added callbacks() for side bar in __init__.py  def toggle_drawer_size in __init__.py

--- jtflask/jtdash/__init__.py
+++ jtflask/jtdash/__init__.py
@@ -7,36 +7,25 @@ from flask import Flask
 from dash_extensions import DeferScript, EventListener
 import dash_mantine_components as dmc
 from dash_iconify import DashIconify
-# import dash_bootstrap_components as dbc
-
 from .layout import html_layout
 from .linecharts import means_to_work
-from .sidebar import sidebar
+from .sidebar import sidebar, collapse_button
 
 external_scripts = [
-    # {"src": "https://unpkg.com/deck.gl@8.9.35/dist.min.js"},
     {"src": "https://unpkg.com/@loaders.gl/i3s@3.3.1/dist/dist.min.js"},
     {"src": "https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.js"},
     {"src": "https://unpkg.com/@turf/turf@6/turf.min.js"},
-    # {"src": "../static/assets/js/arcgis-defer.js", "defer": True},
-    # {"src": "{{ url_for('static', filename='node_modules/@loaders.gl/i3s/dist/index.js') }}"}
 ]
+
 external_stylesheets = [
     {
         "src": "https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.css",
         "rel": "stylesheet",
     },
-    # {
-    #     "href": "https://js.arcgis.com/4.29/esri/themes/light/main.css",
-    #     "rel": "stylesheet",
-    #     "type": "text/css",
-    # }
 ]
-
 
 def init_dashboard(server: Flask):
     """Create a Plotly Dash dashboard within a running Flask jtflask."""
-    # Embed a Dash app into a Flask `server`
     dash_app = Dash(
         server=server,
         routes_pathname_prefix="/jtdash/",
@@ -48,58 +37,39 @@ def init_dashboard(server: Flask):
         assets_ignore=".*defer.js$",
     )
     dash_app.index_string = html_layout
-    # dmc_card = dmc.Card(
-    #     children=[
-    #         html.Div(children="Means to Work", id="means-to-work-title"),
-    #         dcc.Graph(
-    #             id="mean-to-work-line-chart",
-    #             responsive=True,
-    #             config={"displayModeBar": False},
-    #         ),
-    #     ],
-    #     withBorder=True,
-    #     shadow="sm",
-    #     radius="md",
-    #     style={
-    #         "width": 500,
-    #         "height": 250,
-    #         "zIndex": 100,
-    #         "position": "absolute",
-    #         "bottom": 20,
-    #         "left": 20,
-    #     },
-    # )
-    # JavaScript event(s) that we want to listen to and what properties to collect.
+
     event = {"event": "click", "props": ["srcElement.parentElement.dataset.geoid20"]}
+
     dash_app.layout = dmc.MantineProvider(
         html.Div(
             [
-                DeferScript(src="../static/assets/js/arcgis-defer.js",),
+                DeferScript(src="../static/assets/js/arcgis-defer.js"),
                 html.Div(id="deckgl-container"),
-                sidebar(),
-                # EventListener(
-                #     html.Div(id="deckgl-container", **{"data-geoid20": ""}),
-                #     events=[event],
-                #     # logging=True,
-                #     id="deckgl-container-el",
-                # ),
-                # dmc_card,
-                # dcc.Input(id="GEOID20", value="", type="text"),
+                sidebar(),  # Include the sidebar drawer
+                # Additional components
             ]
         )
     )
 
-    # @callback(
-    #     Output("mean-to-work-line-chart", "figure"),
-    #     Input("deckgl-container-el", "n_events"),
-    #     State("deckgl-container-el", "event"),
-    # )
-    # def update_means_to_work_line_chart(n_events, e):
-    #     if e is None:
-    #         raise PreventUpdate()
-    #     print("callback triggered!")
-    #     print(e)
-    #     print(type(e["srcElement.parentElement.dataset.geoid20"]))
-    #     return means_to_work(e["srcElement.parentElement.dataset.geoid20"])
+    @dash_app.callback(
+        [Output("drawer", "size"),
+         Output("sidebar-col1", "style"),
+         Output("collapse-button-container", "style"),
+         Output("collapse-icon", "icon")],
+        Input("collapse-button", "n_clicks"),
+        State("drawer", "size"),
+        prevent_initial_call=True,
+    )
+    def toggle_drawer_size(n_clicks, current_size):
+        if current_size == "80px":
+            return ("15px", {"display": "none"},
+                    {"height": "100%", "width": "100%", "backgroundColor": "lightgray", "display": "flex",
+                     "alignItems": "center", "justifyContent": "center"},
+                    "akar-icons:chevron-right")
+        else:
+            return ("80px", {"display": "block"},
+                    {"height": "100%", "width": "15px", "backgroundColor": "lightgray", "display": "flex",
+                     "alignItems": "center", "justifyContent": "center"},
+                    "akar-icons:chevron-left")
 
     return dash_app.server
