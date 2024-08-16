e51af42c1394d3b5c4cc4f4b02bccbc8496cf7d6
Franklin Pinnock <pinnockf@gmail.com>
2024-08-16 16:24:54 -0400

    Send building selections to /selections flask route

--- jtflask/routes.py
+++ jtflask/routes.py
@@ -5,3 +5,10 @@ from flask import current_app as app
 @app.route('/')
 def home():
     return render_template('index.html', base_url=request.base_url)
+
+@app.route('/selections',  methods=['POST'])
+def handle_selections():
+     if request.method == 'POST':
+        request_body = request.get_json()
+        selections = request_body[selections]
+        return '''<h1>Building Selections: {}</h1>'''.format(selections)
