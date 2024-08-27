0d02da33350f601c424aafcfa5b8f7afb7d073a3
Changjie Chen <chj.chen@Hotmail.com>
2024-08-27 16:53:23 -0400

    add splash related definitions

--- jtflask/jtdash/layout.py
+++ jtflask/jtdash/layout.py
@@ -6,8 +6,21 @@ html_layout = '''
         <title>{%title%}</title>
         {%favicon%}
         {%css%}
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
     </head>
     <body>
+        <div class='splash'>
+            <div class='logo'>
+                <div class="logo-text">
+                    <h1 class='animate__animated animate__fadeIn animate__delay-1s'>Jax</h1>
+                    <h1 class='animate__animated animate__fadeIn animate__delay-3s'>Twin.</h1>
+                </div>
+                <img class='logo-image' src='/jtdash/assets/images/splash-logo-image.png'/>
+            </div>
+        </div>
         {%app_entry%}
         <footer>
             {%config%}
