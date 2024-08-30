3d26619ee4afda11e911dd74cb52d8f01ea958cd
Changjie Chen <chj.chen@Hotmail.com>
2024-08-30 15:26:26 -0400

    pull from development_rpj

---
+++ jtflask/jtdash/arcgis_JS_tools.py
@@ -0,0 +1,21 @@
+import dash_mantine_components as dmc
+from dash import html, dcc
+
+
+def get_arcgis_sketch_card():
+    # ArcGIS Sketch Tool Card
+    arcgis_sketch_card = dmc.Card(
+        children=[
+            dmc.Text("ArcGIS Sketch Tool", size="lg", className="chartLabel"),
+            html.Div(id="arcgis-sketch-container",
+                     style={"height": "100px"})
+        ],
+        withBorder=True,
+        shadow="sm",
+        radius="md",
+        style={"margin": "10px"},
+        id="arcgis_sketch_card",
+        className="cardChart"
+    )
+
+    return arcgis_sketch_card
---
+++ jtflask/jtdash/statshovercards.py
@@ -0,0 +1,59 @@
+from dash import html
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+
+def create_hover_card(icon, text1, text2, card_id):
+    return dmc.HoverCard(
+        children=[
+            dmc.Card(
+                children=[
+                    dmc.Group(
+                        children=[
+                            html.Div(
+                            DashIconify(icon=icon, width=10, className="cardIcon"),
+                                className="cardIconContainer",
+
+                            ),
+                            dmc.Stack(
+                                children=[
+                                    dmc.Text(text1, size="xs", className="cardLabel"),
+                                    dmc.Text(text2, size="lg", className="cardValue")
+                                ],
+                                gap="0px"
+
+                            )
+                        ],
+                        align="center",
+                        id="hover-card-group"
+
+                    )
+                ],
+                shadow="sm",
+                padding="md",
+                radius="md",
+                withBorder=True,
+                className="statsCard",
+                id=card_id,
+
+            )
+        ],
+    )
+
+hover_cards = html.Div(
+    children=[
+        create_hover_card("oi:people", "People impacted", "1,347", "hover-card-1"),
+        create_hover_card("flowbite:dollar-outline", "Property value", "$120 M", "hover-card-2"),
+        create_hover_card("fa-solid:road", "Road mileage", "85", "hover-card-3"),
+        create_hover_card("clarity:building-solid", "Buildings", "835", "hover-card-4")
+    ],
+    style={"position": "absolute", "top": "10px", "left": "10px", "zIndex": 1000}
+)
+
+# Affix
+stats_hover_card = dmc.Affix(
+    children=hover_cards,
+    position={"top": 0, "right": 250},
+    zIndex=1000,
+    id="stats-hover-card-affix"
+)
+
