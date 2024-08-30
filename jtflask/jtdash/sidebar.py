8dea11678e2cd9f895ba7a61f8a5a3f73aa7acc4
Changjie Chen <chj.chen@Hotmail.com>
2024-08-30 16:14:16 -0400

    modify to follow black format

--- jtflask/jtdash/sidebar.py
+++ jtflask/jtdash/sidebar.py
@@ -4,8 +4,14 @@ from dash_iconify import DashIconify
 from .charts import create_charts
 from .arcgis_JS_tools import get_arcgis_sketch_card
 
+
 def get_icon(icon, icon_id=None):
-    return DashIconify(icon=icon, id=icon_id) if icon_id else DashIconify(icon=icon)
+    return (
+        DashIconify(icon=icon, id=icon_id)
+        if icon_id
+        else DashIconify(icon=icon)
+    )
+
 
 def get_sidebar_components():
     charts = create_charts()
@@ -16,14 +22,14 @@ def get_sidebar_components():
             get_icon("solar:box-minimalistic-bold", "sidebar-brand-logo"),
             html.P(
                 children=[
-                    html.Span('Jax', style={"font-weight": "bolder"}),
-                    html.Span('Twin', style={"font-weight": "400"}),
+                    html.Span("Jax", style={"font-weight": "bolder"}),
+                    html.Span("Twin", style={"font-weight": "400"}),
                 ],
-                id="sidebar-brand-logo-text"
-            )
+                id="sidebar-brand-logo-text",
+            ),
         ],
         className="sidebar-brand sidebar-item",
-        id="sidebar-brand"
+        id="sidebar-brand",
     )
 
     sidebar_main = html.Div(
@@ -31,7 +37,9 @@ def get_sidebar_components():
             dmc.NavLink(
                 id="open-charts-drawer-link",
                 # leftSection=get_icon("fluent:data-pie-20-regular", "open-charts"),
-                leftSection=DashIconify(icon="fluent:data-pie-20-regular", width="24px"),
+                leftSection=DashIconify(
+                    icon="fluent:data-pie-20-regular", width="24px"
+                ),
                 className="sidebar-icon",
                 **{"data-position": "center"},
             ),
@@ -39,43 +47,42 @@ def get_sidebar_components():
                 id="open-arcgis-drawer-link",
                 leftSection=get_icon("solar:routing-2-linear", "open-arcgis"),
                 className="sidebar-icon",
-                **{"data-position": "center"}
+                **{"data-position": "center"},
             ),
             dmc.NavLink(
                 id="buildings-link",
                 leftSection=get_icon("ph:buildings"),
                 className="sidebar-icon",
-                **{"data-position": "center"}
+                **{"data-position": "center"},
             ),
             dmc.NavLink(
                 id="clipboard-link",
                 leftSection=get_icon("solar:clipboard-text-linear"),
                 className="sidebar-icon",
-                **{"data-position": "center"}
+                **{"data-position": "center"},
             ),
             dmc.NavLink(
                 id="layers-link",
                 leftSection=get_icon("solar:layers-minimalistic-linear"),
                 className="sidebar-icon",
-                **{"data-position": "center"}
+                **{"data-position": "center"},
             ),
             dmc.NavLink(
                 id="list-link",
                 leftSection=get_icon("la:list-ul"),
                 className="sidebar-icon",
-                **{"data-position": "center"}
+                **{"data-position": "center"},
             ),
             dmc.NavLink(
                 id="maps-link",
                 leftSection=get_icon("hugeicons:maps-square-01"),
                 className="sidebar-icon",
-                **{"data-position": "center"}
+                **{"data-position": "center"},
             ),
         ],
         className="sidebar-main sidebar-item",
         id="sidebar-main",
-        style={"z": "20px"}
-
+        style={"z": "20px"},
     )
 
     collapse_button = dmc.Button(
@@ -88,33 +95,42 @@ def get_sidebar_components():
     collapse_button_container = html.Div(
         collapse_button,
         id="collapse-button-container",
-        className="sidebar-item"
+        className="sidebar-item",
     )
 
     scrollable_div_hidden = html.Div(
-        id="scrollable-div-hidden",
-        className="scrollable-div"
+        id="scrollable-div-hidden", className="scrollable-div"
     )
 
     scrollable_div_charts = html.Div(
-        children=charts,
-        id="chart_scrollable_div",
-        className="scrollable-div"
+        children=charts, id="chart_scrollable_div", className="scrollable-div"
     )
 
     scrollable_div_tools = html.Div(
         children=arcgis_tools,
         id="arcgistools_scrollable_div",
-        className="scrollable-div hidden"
+        className="scrollable-div hidden",
     )
 
-    return sidebar_brand, sidebar_main, collapse_button_container, scrollable_div_charts, scrollable_div_tools
+    return (
+        sidebar_brand,
+        sidebar_main,
+        collapse_button_container,
+        scrollable_div_charts,
+        scrollable_div_tools,
+    )
 
 
-def sidebar(dash_app, sidebar_brand, sidebar_main, collapse_button_container, scrollable_div_charts, scrollable_div_tools):
+def sidebar(
+    dash_app,
+    sidebar_brand,
+    sidebar_main,
+    collapse_button_container,
+    scrollable_div_charts,
+    scrollable_div_tools,
+):
     scrollable_div_drawer = dmc.Drawer(
-        children=[scrollable_div_charts,
-                  scrollable_div_tools],
+        children=[scrollable_div_charts, scrollable_div_tools],
         id="chart_scrollable_drawer",
         className="",
         padding="2",
@@ -136,10 +152,13 @@ def sidebar(dash_app, sidebar_brand, sidebar_main, collapse_button_container, sc
         [
             html.Div(
                 [sidebar_brand, sidebar_main, collapse_button_container],
-                id="sidebar-col1"
+                id="sidebar-col1",
             ),
             scrollable_div_drawer,
-            dcc.Store(id='drawer-content-store', data={'charts': 'charts', 'tools': 'tools'})
+            dcc.Store(
+                id="drawer-content-store",
+                data={"charts": "charts", "tools": "tools"},
+            ),
         ],
         id="drawer-body-grid",
     )
@@ -148,4 +167,3 @@ def sidebar(dash_app, sidebar_brand, sidebar_main, collapse_button_container, sc
         children=[drawer_content],
         id="drawer",
     )
-
