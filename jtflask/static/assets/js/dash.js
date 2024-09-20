6db5f11f29022f0e69baa0613ef68baf4f1d043a
Changjie Chen <chj.chen@Hotmail.com>
2024-09-20 14:03:58 -0400

    add svg hover effect function

--- jtflask/static/assets/js/dash.js
+++ jtflask/static/assets/js/dash.js
@@ -14,7 +14,41 @@ const JTDash = (() => {
         }
     });
 
+    const svgOnHover = (svgPath, svgId, svgContainerId, colorOnFocus, colorOutFocus) => {
+        // Fetch the SVG file and insert it into the DOM
+        fetch(svgPath)
+            .then(response => response.text())
+            .then(svgContent => {
+                document.getElementById(svgId).innerHTML = svgContent;
+
+                // Now the SVG is in the DOM, and you can manipulate it
+                const svgPaths = document.querySelectorAll(`#${svgId} path`);
+
+                document.getElementById(svgContainerId)
+                    .addEventListener('mouseover', () => {
+                        svgPaths.forEach((path) => {
+                            ['fill', 'stroke'].forEach(attr => {
+                                if (path.getAttribute(attr)) {
+                                    path.setAttribute(attr, colorOnFocus);  // Change both fill and stroke to white if they exist
+                                }
+                            });
+                        });
+                    });
+                document.getElementById(svgContainerId)
+                    .addEventListener('mouseout', () => {
+                        svgPaths.forEach((path) => {
+                            ['fill', 'stroke'].forEach(attr => {
+                                if (path.getAttribute(attr)) {
+                                    path.setAttribute(attr, colorOutFocus);  // Change both fill and stroke to white if they exist
+                                }
+                            });
+                        });
+                    });
+            });
+    };
+
     return {
-        sendToDash: sendToDash
+        sendToDash: sendToDash,
+        svgOnHover: svgOnHover
     };
 })();  // IIFE
