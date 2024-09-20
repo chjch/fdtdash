2c35108e4cae415004517e754412a0d00e1665a0
Changjie Chen <chj.chen@Hotmail.com>
2024-09-20 10:36:50 -0400

    upgrade dash to 2.17.1

---
+++ jtflask/jtdash/basemap_gallery.py
@@ -0,0 +1,23 @@
+import dash_mantine_components as dmc
+from dash import html
+
+def get_basemap_gallery():
+    basemap_gallery_card = dmc.Card(
+        id="basemap-gallery-card",
+        withBorder=True,
+        shadow="sm",
+        radius="md",
+        children=[
+            dmc.Text("Basemap Gallery", size="lg", className="chartLabel"),
+            # Basemap gallery widget will render to this div from main.defer
+            html.Div(id="basemap-gallery-card-content"),
+        ],
+    )
+
+    basemap_gallery = html.Div(
+        id="drawer-basemap-gallery",
+        className="scrollable-div hidden",
+        children=[basemap_gallery_card],
+    )
+    
+    return basemap_gallery
