2018acc3869f47fe8214d5b5566995e416d5fad6
Changjie Chen <chj.chen@Hotmail.com>
2024-09-20 14:05:51 -0400

    simplify sidebar callback

--- jtflask/jtdash/callbacks.py
+++ jtflask/jtdash/callbacks.py
@@ -1,70 +1,12 @@
-import dash
-from dash import Input, Output, State, no_update
+from dash import Input, Output, State, no_update, ctx
 
 
 def register_callbacks(dashboard):
     @dashboard.callback(
-        Input("chart-data-store", "data"),
-        prevent_initial_call=True
+        Input("chart-data-store", "data"), prevent_initial_call=True
     )
     def print_data(data):
         print(f"printing from callback {data}")
-
-    @dashboard.callback(
-        [
-            Output("chart_scrollable_div", "className"),
-            Output("arcgistools_scrollable_div", "className"),
-            Output("chart_scrollable_div", "style"),
-            Output("arcgistools_scrollable_div", "style"),
-            Output("drawer-content-store", "data"),
-        ],
-        [
-            Input("open-charts-drawer-link", "n_clicks"),
-            Input("open-arcgis-drawer-link", "n_clicks"),
-        ],
-        [State("drawer-content-store", "data")],
-        prevent_initial_call=True,
-    )
-    def update_drawer_content(charts_click, tools_click, current_content):
-        ctx = dash.callback_context
-
-        if not ctx.triggered:
-            return no_update, no_update, no_update, no_update, no_update
-
-        trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
-
-        # List of sidebar icon NavLinks that should trigger the callback
-        valid_navlinks = ["open-charts-drawer-link", "open-arcgis-drawer-link"]
-
-        # Only execute if the trigger is a sidebar icon NavLink
-        if trigger_id not in valid_navlinks:
-            return no_update, no_update, no_update, no_update, no_update
-
-        if (
-            trigger_id == "open-charts-drawer-link"
-            and current_content != "charts"
-        ):
-            return (
-                "slide-in",
-                "slide-out",
-                {"display": "block"},
-                {"display": "none"},
-                "charts",
-            )
-
-        elif (
-            trigger_id == "open-arcgis-drawer-link"
-            and current_content != "tools"
-        ):
-            return (
-                "slide-out",
-                "slide-in",
-                {"display": "none"},
-                {"display": "block"},
-                "tools",
-            )
-
-        return no_update, no_update, no_update, no_update, no_update
 
     # Collapse Sidebar Callback
     @dashboard.callback(
@@ -96,3 +38,61 @@ def register_callbacks(dashboard):
     )
     def select_value(value):
         return value
+
+    # Sidebar Button Click Callback
+    @dashboard.callback(
+        output={
+            "charts_class_name": Output("chart_scrollable_div", "className"),
+            "tools_className": Output(
+                "arcgistools_scrollable_div", "className"
+            ),
+            "basemap_gallery_className": Output(
+                "drawer-basemap-gallery", "className"
+            ),
+        },
+        inputs=[
+            Input("charts-toggle-button", "n_clicks"),
+            Input("arcgis-tools-toggle-button", "n_clicks"),
+            Input("buildings-toggle-button", "n_clicks"),
+            Input("clipboard-toggle-button", "n_clicks"),
+            Input("layers-toggle-button", "n_clicks"),
+            Input("legend-toggle-button", "n_clicks"),
+            Input("basemap-gallery-toggle-button", "n_clicks"),
+        ],
+    )
+    def handle_sidebar_icon_click(*inputs):
+        # Get id of clicked button
+        clicked_button_id = ctx.triggered_id if not None else ""
+
+        # Update class of clicked button to slide-in animation
+        def handle_button_click(button_id):
+            if button_id == clicked_button_id:
+                return f"{button_id} slide-in"
+            else:
+                return f"{button_id} hidden"
+
+        outputs = map(
+            handle_button_click,
+            [
+                "charts-toggle-button",
+                "arcgis-tools-toggle-button",
+                "buildings-toggle-button",
+                "clipboard-toggle-button",
+                "layers-toggle-button",
+                "legend-toggle-button",
+                "basemap-gallery-toggle-button",
+            ],
+        )
+        outputs = list(outputs)
+
+        charts_class_name = (
+            "charts-toggle-button slide-in"
+            if all(n_clicks is None for n_clicks in inputs)
+            else outputs[0]
+        )
+
+        return {
+            "charts_class_name": charts_class_name,
+            "tools_className": outputs[1],
+            "basemap_gallery_className": outputs[6],
+        }
