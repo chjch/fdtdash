0fd1a5541908bbf80b7571019e4ee140c1ceab33
Changjie Chen <chj.chen@Hotmail.com>
2024-09-20 14:01:27 -0400

    initial commit

--- jtflask/jtdash/utils.py
+++ jtflask/jtdash/utils.py
@@ -1,4 +1,9 @@
 from dash_iconify import DashIconify
 
+
 def get_icon(icon, icon_id=None):
-    return DashIconify(icon=icon, id=icon_id) if icon_id else DashIconify(icon=icon)
+    return (
+        DashIconify(icon=icon, id=icon_id, height=16, width=16)
+        if icon_id
+        else DashIconify(icon=icon, height=16, width=16)
+    )
