a8a39565951361b6a23600d16385d744cb6d9233
Changjie Chen <chj.chen@Hotmail.com>
2024-09-30 13:35:29 -0400

    add attribute query

--- jtflask/static/assets/js/query.js
+++ jtflask/static/assets/js/query.js
@@ -37,6 +37,7 @@ const JTSpatialQuery = (() => {
             return layerView.layer.queryFeatures(query).then((results) => {
                 if (results.features.length > 0) {
                     const attributes = results.features.map(feature => feature.attributes);
+                    // noinspection JSUnresolvedVariable
                     const objectIds = attributes.map(attr => attr.OBJECTID);
                     JTHighlight.highlightBuildings(objectIds, layerView);
                     return attributes;
@@ -52,5 +53,62 @@ const JTSpatialQuery = (() => {
     return {
         objectIds: objectIds,
         attributes: attributes
+    };
+})(); // IIFE
+
+const JTAttributeQuery = (() => {
+    // const byField = (layer, fieldName, outFields=["*"]) => {
+    //     const query = layer.createQuery();
+    //     query.where = `${fieldName} IS NOT NULL`; // not null
+    //     query.returnGeometry = false;
+    //     query.outFields = outFields;
+    //     query.maxRecordCountFactor = 5;
+    //
+    //     return layer.queryFeatures(query).then((results) => {
+    //         // console.log(`Fetched ${attributes.length} records with field: ${fieldName}`);
+    //         return results.features.map(feature => feature.attributes);
+    //     }).catch((error) => {
+    //         console.error(`Error querying by field name ${fieldName}:`, error);
+    //         return [];
+    //     });
+    // };
+    //
+    // const byFieldValue = (layer, fieldName, fieldValue, outFields=["*"]) => {
+    //     const query = layer.createQuery();
+    //     query.where = `${fieldName} = '${fieldValue}'`;
+    //     query.returnGeometry = false;
+    //     query.outFields = outFields;
+    //     query.maxRecordCountFactor = 10;
+    //
+    //     return layer.queryFeatures(query).then((results) => {
+    //         // console.log(`Fetched ${attributes.length} records where ${fieldName} = ${fieldValue}`);
+    //         return results.features.map(feature => feature.attributes);
+    //     }).catch((error) => {
+    //         console.error(`Error querying by field value ${fieldName}=${fieldValue}:`, error);
+    //         return [];
+    //     });
+    // };
+
+    const byField = (layer, fieldName, fieldValue = null, outFields = ["*"]) => {
+        const query = layer.createQuery();
+
+        // If fieldValue is provided, check for equality, otherwise check for IS NOT NULL
+        query.where = fieldValue ? `${fieldName} = '${fieldValue}'` : `${fieldName} IS NOT NULL`;
+        query.returnGeometry = false;
+        query.outFields = outFields;
+        query.maxRecordCountFactor = 10;
+
+        return layer.queryFeatures(query)
+            .then((results) => {
+                return results.features.map(feature => feature.attributes);
+            })
+            .catch((error) => {
+                console.error(`Error querying by field ${fieldName}${fieldValue ? `=${fieldValue}` : ''}:`, error);
+                return [];
+            });
+    };
+
+    return {
+        byField: byField
     };
 })(); // IIFE
