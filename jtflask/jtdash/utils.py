450bb5bc9c0f33a79c069b0d0ce38a2cd1792762
Changjie Chen <chj.chen@Hotmail.com>
2024-10-20 22:47:06 -0400

    remove predefined icon size

--- jtflask/jtdash/utils.py
+++ jtflask/jtdash/utils.py
@@ -3,7 +3,7 @@ from dash_iconify import DashIconify
 
 def get_icon(icon, icon_id=None):
     return (
-        DashIconify(icon=icon, id=icon_id, height=16, width=16)
+        DashIconify(icon=icon, id=icon_id)
         if icon_id
-        else DashIconify(icon=icon, height=16, width=16)
+        else DashIconify(icon=icon)
     )
