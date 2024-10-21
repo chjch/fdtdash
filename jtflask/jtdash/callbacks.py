d08a96e4224feee820036a456f5dd67edce921a2
Changjie Chen <chj.chen@Hotmail.com>
2024-10-20 22:51:23 -0400

    add clientside_callback; remove sidebar callbacks and handle them via clientside

--- jtflask/jtdash/callbacks.py
+++ jtflask/jtdash/callbacks.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
-from dash import Input, Output, State, no_update, ctx
+from dash import Input, Output, State, no_update, ctx, ClientsideFunction
+
+from .sidebar import NAVBAR_BUTTONS, NAVBAR_PANELS
 
 
 def register_callbacks(dashboard):
@@ -168,81 +170,78 @@ def register_callbacks(dashboard):
 
     # Collapse Sidebar Callback
     @dashboard.callback(
-        [
-            Output("chart_scrollable_drawer", "opened"),
-            Output("collapse-icon", "icon"),
-        ],
+        Output("collapse-icon", "icon"),
         Input("collapse-button", "n_clicks"),
-        State("chart_scrollable_drawer", "opened"),
         prevent_initial_call=True,
     )
-    def toggle_sidebar_drawer(n_clicks, is_open):
+    def toggle_sidebar_drawer(n_clicks):
+        is_open = True if n_clicks % 2 == 1 else False
         new_icon = (
             "heroicons:chevron-double-right-16-solid"
             if is_open
             else "heroicons:chevron-double-left-16-solid"
         )
-        return not is_open, new_icon
+        return new_icon
 
     # Sidebar Button Click Callback
-    @dashboard.callback(
-        output={
-            "charts_class_name": Output("chart_scrollable_div", "className"),
-            "tools_className": Output(
-                "arcgistools_scrollable_div", "className"
-            ),
-            "scrollable_div_bulding_className": Output("scrollable_div_bulding_stats", "className"),
-            "basemap_gallery_className": Output(
-                "drawer-basemap-gallery", "className"
-            ),
-        },
-        inputs=[
-            Input("charts-toggle-button", "n_clicks"),
-            Input("arcgis-tools-toggle-button", "n_clicks"),
-            Input("buildings-toggle-button", "n_clicks"),
-            Input("clipboard-toggle-button", "n_clicks"),
-            Input("layers-toggle-button", "n_clicks"),
-            Input("legend-toggle-button", "n_clicks"),
-            Input("basemap-gallery-toggle-button", "n_clicks"),
-        ],
-    )
-    def handle_sidebar_icon_click(*inputs):
-        # Get id of clicked button
-        clicked_button_id = ctx.triggered_id if not None else ""
-
-        # Update class of clicked button to slide-in animation
-        def handle_button_click(button_id):
-            if button_id == clicked_button_id:
-                return f"{button_id} mantine-Drawer-body-item slide-in"
-            else:
-                return f"{button_id} mantine-Drawer-body-item hidden "
-
-        outputs = map(
-            handle_button_click,
-            [
-                "charts-toggle-button",
-                "arcgis-tools-toggle-button",
-                "buildings-toggle-button",
-                "clipboard-toggle-button",
-                "layers-toggle-button",
-                "legend-toggle-button",
-                "basemap-gallery-toggle-button",
-            ],
-        )
-        outputs = list(outputs)
-
-        charts_class_name = (
-            "charts-toggle-button mantine-Drawer-body-item slide-in"
-            if all(n_clicks is None for n_clicks in inputs)
-            else outputs[0]
-        )
-
-        return {
-            "charts_class_name": charts_class_name,
-            "tools_className": outputs[1],
-            "scrollable_div_bulding_className": outputs[2],
-            "basemap_gallery_className": outputs[6],
-        }
+    # @dashboard.callback(
+    #     output={
+    #         "charts_class_name": Output("chart_scrollable_div", "className"),
+    #         "tools_className": Output(
+    #             "arcgistools_scrollable_div", "className"
+    #         ),
+    #         "scrollable_div_bulding_className": Output("scrollable_div_bulding_stats", "className"),
+    #         "basemap_gallery_className": Output(
+    #             "drawer-basemap-gallery", "className"
+    #         ),
+    #     },
+    #     inputs=[
+    #         Input("charts-toggle-button", "n_clicks"),
+    #         Input("arcgis-tools-toggle-button", "n_clicks"),
+    #         Input("buildings-toggle-button", "n_clicks"),
+    #         Input("clipboard-toggle-button", "n_clicks"),
+    #         Input("layers-toggle-button", "n_clicks"),
+    #         Input("legend-toggle-button", "n_clicks"),
+    #         Input("basemap-gallery-toggle-button", "n_clicks"),
+    #     ],
+    # )
+    # def handle_sidebar_icon_click(*inputs):
+    #     # Get id of clicked button
+    #     clicked_button_id = ctx.triggered_id if not None else ""
+    #
+    #     # Update class of clicked button to slide-in animation
+    #     def handle_button_click(button_id):
+    #         if button_id == clicked_button_id:
+    #             return f"{button_id} mantine-Drawer-body-item slide-in"
+    #         else:
+    #             return f"{button_id} mantine-Drawer-body-item hidden "
+    #
+    #     outputs = map(
+    #         handle_button_click,
+    #         [
+    #             "charts-toggle-button",
+    #             "arcgis-tools-toggle-button",
+    #             "buildings-toggle-button",
+    #             "clipboard-toggle-button",
+    #             "layers-toggle-button",
+    #             "legend-toggle-button",
+    #             "basemap-gallery-toggle-button",
+    #         ],
+    #     )
+    #     outputs = list(outputs)
+    #
+    #     charts_class_name = (
+    #         "charts-toggle-button mantine-Drawer-body-item slide-in"
+    #         if all(n_clicks is None for n_clicks in inputs)
+    #         else outputs[0]
+    #     )
+    #
+    #     return {
+    #         "charts_class_name": charts_class_name,
+    #         "tools_className": outputs[1],
+    #         "scrollable_div_bulding_className": outputs[2],
+    #         "basemap_gallery_className": outputs[6],
+    #     }
 
     # Scenario Chart callback
     @dashboard.callback(
@@ -258,3 +257,24 @@ def register_callbacks(dashboard):
     )
     def select_value(value):
         return value
+
+    dashboard.clientside_callback(
+        ClientsideFunction(
+            namespace="clientside",
+            function_name="sendToDash"
+        ),
+        Input("chart-data-store", "data"),
+    )
+
+    dashboard.clientside_callback(
+        ClientsideFunction(
+            namespace="clientside",
+            function_name="handleNavbarButtonClick"
+        ),
+        # Dummy Output. Not used. Not needed in newer versions of dash
+        Output("navbar-control", "className"),
+        [
+            Input(button, "n_clicks")
+            for button in NAVBAR_BUTTONS.values()
+        ]
+    )
