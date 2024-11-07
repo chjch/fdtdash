80f441fa12d99901cda81d3a7b4d1d3e4eea62ea
Changjie Chen <chj.chen@Hotmail.com>
2024-11-07 16:13:43 -0500

    update urls

--- jtflask/routes.py
+++ jtflask/routes.py
@@ -7,32 +7,32 @@ def home():
     return render_template('index.html', base_url=request.base_url)
 
 
-@app.route('/jtdash/selection', methods=['POST'])
-def handle_selections():
-    if request.method == 'POST':
-        request_body = request.get_json()
-        # Process the selections data as needed
-        print(f"Received selection: {request_body}")
+# @app.route('/jtdash/selection', methods=['POST'])
+# def handle_selections():
+#     if request.method == 'POST':
+#         request_body = request.get_json()
+#         # Process the selections data as needed
+#         print(f"Received selection: {request_body}")
+#
+#         # Return a nicely formatted JSON response
+#         return app.response_class(
+#             response=jsonify({"status": "success", "selection": request_body}).get_data(as_text=True),
+#             status=200,
+#             mimetype='application/json',
+#             headers={'Content-Type': 'application/json'}
+#         )
 
-        # Return a nicely formatted JSON response
-        return app.response_class(
-            response=jsonify({"status": "success", "selection": request_body}).get_data(as_text=True),
-            status=200,
-            mimetype='application/json',
-            headers={'Content-Type': 'application/json'}
-        )
 
-
-@app.route('/spatial')
+@app.route('/spatial/')
 def serve_spatial():
     return send_from_directory('static', 'SpatialAwareness.html')
 
 
-@app.route('/temporal')
+@app.route('/temporal/')
 def serve_temporal():
     return send_from_directory('static', 'TemporalAwareness.html')
 
 
-@app.route('/scene')
+@app.route('/scene/')
 def serve_scene():
     return send_from_directory('static', 'SceneView.html')
